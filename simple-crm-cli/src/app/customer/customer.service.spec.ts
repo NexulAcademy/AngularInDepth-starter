e690936b6874a798b309c0e350f822ccb708288f
Michael Lang <michael.lang@nexul.com>
2022-10-24 09:19:14 -0500

    69: fix service tests

--- simple-crm-cli/src/app/customer/customer.service.spec.ts
+++ simple-crm-cli/src/app/customer/customer.service.spec.ts
@@ -1,16 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
+import { CustomerMockService } from './customer-mock.service';
 
 import { CustomerService } from './customer.service';
 
 describe('CustomerService', () => {
-  let service: CustomerService;
+  let injector: TestBed;
+  let service: CustomerMockService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CustomerService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CustomerMockService]
+    });
+    injector = getTestBed();
+    service = TestBed.inject(CustomerMockService);
+    httpMock = injector.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // ensures there are no outstanding requests between tests.
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('search should work', () => {
     expect(service).toBeTruthy();
   });
 });
